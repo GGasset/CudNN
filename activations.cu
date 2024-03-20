5da89a2c31ef990a93e47be8523cf6c73d676280
Germán <GassetGerman@gmail.com>
2024-03-20 20:29:40 +0100

    added sigmoid activation function

---
+++ activations.cu
@@ -0,0 +1,29 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <cmath>
+
+enum ActivationFunctions
+{
+	sigmoid
+};
+
+__global__ void sigmoid_activation(
+	data_t *activations, size_t activations_start, size_t layer_activation_start, short write_activation,
+	data_t *execution_values, size_t execution_values_start, size_t execution_values_layer_start, size_t execution_values_per_neuron,
+	size_t neuron_execution_values_read, size_t neuron_execution_values_write, short write_execution_values
+)
+{
+	size_t neuron_execution_values_start = neuron_execution_values_start + execution_values_layer_start + execution_values_per_neuron * threadIdx.x;
+	data_t activation = 1 / (1 + exp(-execution_values[neuron_execution_values_start + neuron_execution_values_read]));
+	if (write_activation)
+	{
+		size_t activations_i = activations_start + layer_activation_start + threadIdx.x;
+		activations[activations_i] = activation;
+	}
+	if (write_execution_values)
+	{
+		size_t execution_values_i = execution_values_start + execution_values_layer_start + execution_values_per_neuron * threadIdx.x + neuron_execution_values_write;
+		activations[execution_values_i] = activation;
+	}
+}

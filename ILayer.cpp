288a64ffe9fd8219735e3c1f6cd866554796a95e
Germán <GassetGerman@gmail.com>
2024-03-20 22:15:07 +0100

    modifed field generation functions

--- ILayer.cpp
+++ ILayer.cpp
@@ -1,1 +1,13 @@
 #include "ILayer.h"
+
+void ILayer::add_neuron(size_t neurons_to_add, size_t connection_count_per_neuron)
+{
+	parameter_t* tmp;
+	tmp = weights;
+	size_t new_weight_count = connection_count + sizeof(parameter_t) * neurons_to_add * connection_count_per_neuron;
+	cudaMalloc(&weights, new_weight_count);
+	cudaMemcpy(weights, tmp, new_weight_count, cudaMemcpyDeviceToDevice);
+	cudaFree(tmp);
+	connection_count += neurons_to_add * connection_count_per_neuron;
+	neuron_count += neurons_to_add;
+}
--- ILayer.h
+++ ILayer.h
@@ -8,6 +8,7 @@
 class ILayer
 {
 public:
+	size_t connection_count = 0;
 	size_t neuron_count = 0;
 	IConnections* connections = 0;
 	parameter_t* weights = 0;
@@ -16,13 +17,22 @@ public:
 	size_t execution_values_layer_start = 0;
 	size_t execution_values_per_neuron = 0;
 
-	void initialize_weights(size_t connection_count)
+	void Initialize_fields(size_t connection_count, size_t neuron_count)
+	{
+		cudaMalloc(&weights, sizeof(parameter_t) * connection_count);
+		generate_random_weights(connection_count);
+		cudaMalloc(&biases, sizeof(parameter_t) * neuron_count);
+		cudaMemset(biases, 1, sizeof(parameter_t) * neuron_count);
+	}
+
+	void generate_random_weights(size_t connection_count, size_t start_i = 0)
 	{
 		curandGenerator_t generator;
 		curandCreateGenerator(&generator, CURAND_RNG_PSEUDO_XORWOW);
-		cudaMalloc(&weights, sizeof(parameter_t) * connection_count);
-		curandGenerateUniform(generator, weights, connection_count);
+		curandGenerateUniform(generator, weights + start_i, connection_count);
 	}
+
+	virtual void add_neuron(size_t neurons_to_add, size_t connection_count_per_neuron) = 0;
 
 	virtual void deallocate()
 	{

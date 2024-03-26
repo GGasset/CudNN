9c2a2d2aad03b65f7c54fda68c73851ffb701882
Germán <GassetGerman@gmail.com>
2024-03-26 13:34:37 +0100

    added basic structure

--- NN.h
+++ NN.h
@@ -1,9 +1,14 @@
+#include "ILayer.h"
+
+#pragma once
 class NN
 {
 private:
+	ILayer *layers;
 	size_t max_layer_count = 0;
 	size_t layer_count = 0;
 
 public:
+	data_t* Execute(data_t* input, data_t* execution_results);
 };
 

8a7ae0f0b5629f7e45b2942e478d481105ad52af
Germán <GassetGerman@gmail.com>
2024-03-26 15:02:05 +0100

    added fields and ::Execute

--- NN.cpp
+++ NN.cpp
@@ -1,6 +1,17 @@
 #include "NN.h"
 
-data_t* Execute(data_t* input, data_t* execution_results, size_t t)
+data_t* NN::Execute(data_t* input, data_t* execution_values, data_t *activations, size_t t, short copy_output_to_host = true)
 {
-
+	cudaMemcpy(activations + t * neuron_count, input, input_length, cudaMemcpyHostToDevice);
+	for (size_t i = 0; i < layer_count; i++)
+	{
+		layers[i]->execute(activations, neuron_count * t, execution_values, execution_value_count * t);
+	}
+	data_t* output = 0;
+	if (copy_output_to_host)
+	{
+		output = new data_t[output_length];
+		cudaMemcpy(output, activations + neuron_count * t + *output_activations_start, output_length, cudaMemcpyDeviceToHost);
+	}
+	return output;
 }
--- NN.h
+++ NN.h
@@ -4,12 +4,16 @@
 class NN
 {
 private:
-	ILayer *layers = 0;
+	ILayer **layers = 0;
 	size_t max_layer_count = 0;
 	size_t layer_count = 0;
+	size_t neuron_count = 0;
+	size_t input_length = 0;
+	size_t output_length = 0;
+	size_t* output_activations_start = 0;
 	size_t execution_value_count = 0;
 
 public:
-	data_t* Execute(data_t* input, data_t* execution_values, size_t t);
+	data_t* Execute(data_t* input, data_t* execution_values, data_t *activations, size_t t, short copy_output_to_host = true);
 };
 

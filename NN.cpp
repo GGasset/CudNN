322a8a9fafb71063c3e33a586d8ea34685411951
Germán <GassetGerman@gmail.com>
2024-03-26 14:01:00 +0100

    minor changes

--- NN.cpp
+++ NN.cpp
@@ -1,1 +1,6 @@
 #include "NN.h"
+
+data_t* Execute(data_t* input, data_t* execution_results, size_t t)
+{
+
+}
--- NN.h
+++ NN.h
@@ -4,11 +4,12 @@
 class NN
 {
 private:
-	ILayer *layers;
+	ILayer *layers = 0;
 	size_t max_layer_count = 0;
 	size_t layer_count = 0;
+	size_t execution_value_count = 0;
 
 public:
-	data_t* Execute(data_t* input, data_t* execution_results, size_t t);
+	data_t* Execute(data_t* input, data_t* execution_values, size_t t);
 };
 

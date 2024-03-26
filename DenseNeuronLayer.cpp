53a9494d93737a990fce3a34f10ea125cb1e4c86
Germán <GassetGerman@gmail.com>
2024-03-26 14:00:02 +0100

    fixed bug

--- DenseNeuronLayer.cpp
+++ DenseNeuronLayer.cpp
@@ -7,5 +7,6 @@ DenseNeuronLayer::DenseNeuronLayer(size_t neuron_count, size_t previous_layer_ne
 	this->activation = activation;
 	this->neuron_count = neuron_count;
 	this->connection_count = neuron_count * previous_layer_length;
+	execution_values_per_neuron = 1;
 	Initialize_fields(previous_layer_length * neuron_count, neuron_count);
 }

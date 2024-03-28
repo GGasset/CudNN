d162fa43ad43687a48f2338211cdc02c262fbd16
Germán <GassetGerman@gmail.com>
2024-03-28 22:15:37 +0100

    fixed bug

--- NN.cpp
+++ NN.cpp
@@ -2,7 +2,7 @@
 
 data_t* NN::Execute(data_t* input, data_t* execution_values, data_t *activations, size_t t, short copy_output_to_host = true)
 {
-	cudaMemcpy(activations + t * neuron_count, input, input_length, cudaMemcpyHostToDevice);
+	cudaMemcpy(activations + t * neuron_count, input + input_length * t, input_length, cudaMemcpyHostToDevice);
 	for (size_t i = 0; i < layer_count; i++)
 	{
 		layers[i]->execute(activations, neuron_count * t, execution_values, execution_value_count * t);

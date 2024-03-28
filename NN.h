a6ef5ee2e4bcd1819c4e4b3bf35a1eb6b1de8114
Germán <GassetGerman@gmail.com>
2024-03-28 22:26:00 +0100

    added field

--- NN.h
+++ NN.h
@@ -12,6 +12,7 @@ private:
 	size_t output_length = 0;
 	size_t* output_activations_start = 0;
 	size_t execution_value_count = 0;
+	size_t gradient_value_count = 0;
 
 public:
 	data_t* Execute(data_t* input, data_t* execution_values, data_t *activations, size_t t, short copy_output_to_host = true);

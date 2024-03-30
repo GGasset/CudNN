0adc34bb1a0d99ebe31d2da1222d7dc9e9737806
Germán <GassetGerman@gmail.com>
2024-03-30 21:15:05 +0100

    Modified bug in constructor

--- DenseNeuronLayer.cpp
+++ DenseNeuronLayer.cpp
@@ -1,6 +1,6 @@
 #include "DenseNeuronLayer.h"
 
-DenseNeuronLayer::DenseNeuronLayer(size_t neuron_count, size_t previous_layer_neuron_i_start, size_t previous_layer_length, ActivationFunctions activation)
+DenseNeuronLayer::DenseNeuronLayer(size_t layer_gradients_start, size_t neuron_count, size_t previous_layer_neuron_i_start, size_t previous_layer_length, ActivationFunctions activation)
 {
 	connections = new DenseConnections(previous_layer_neuron_i_start, previous_layer_length);
 	this->activation = activation;
@@ -8,7 +8,7 @@ DenseNeuronLayer::DenseNeuronLayer(size_t neuron_count, size_t previous_layer_ne
 	this->connection_count = neuron_count * previous_layer_length;
 	execution_values_per_neuron = 1;
 	
-	size_t neuron_gradient_i = 0;
+	size_t neuron_gradient_i = layer_gradients_start;
 	size_t* neuron_gradients_starts = new size_t[neuron_count];
 	for (size_t i = 0; i < neuron_count; i++)
 	{
@@ -16,7 +16,7 @@ DenseNeuronLayer::DenseNeuronLayer(size_t neuron_count, size_t previous_layer_ne
 		neuron_gradient_i += previous_layer_length + 1;
 	}
 	cudaMalloc(&this->neuron_gradients_starts, neuron_count * sizeof(size_t));
-	cudaMemcpy(this->neuron_gradients_starts, neuron_gradients_starts, neuron_count * sizeof(size_t));
+	cudaMemcpy(this->neuron_gradients_starts, neuron_gradients_starts, neuron_count * sizeof(size_t), cudaMemcpyHostToDevice);
 	delete[] neuron_gradients_starts;
 
 	layer_gradient_count = connection_count + neuron_count;
--- DenseNeuronLayer.h
+++ DenseNeuronLayer.h
@@ -5,6 +5,6 @@
 class DenseNeuronLayer : public NeuronLayer
 {
 public:
-	DenseNeuronLayer(size_t neuron_count, size_t previous_layer_start, size_t previous_layer_length, ActivationFunctions activation);
+	DenseNeuronLayer(size_t layer_gradients_start, size_t neuron_count, size_t previous_layer_neuron_i_start, size_t previous_layer_length, ActivationFunctions activation);
 };
 

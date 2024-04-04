ea811f4a758ccb5839569ce2f15143899ac78098
Germán <GassetGerman@gmail.com>
2024-04-04 16:32:16 +0200

    finished tanh activation

--- neuron_operations.cu
+++ neuron_operations.cu
@@ -34,7 +34,19 @@ __global__ void tanh_activation(
 	size_t neuron_execution_values_read, size_t neuron_execution_values_write, short write_execution_values
 )
 {
-	
+	size_t neuron_execution_values_start = execution_values_start + execution_values_layer_start + execution_values_per_neuron * threadIdx.x;
+	data_t x = execution_values[neuron_execution_values_start + neuron_execution_values_read];
+	data_t activation = (exp(x) - exp(-x)) / (exp(x) + exp(-x));
+	if (write_activation)
+	{
+		size_t activations_i = activations_start + layer_activation_start + threadIdx.x;
+		activations[activations_i] = activation;
+	}
+	if (write_execution_values)
+	{
+		size_t execution_values_i = execution_values_start + execution_values_layer_start + execution_values_per_neuron * threadIdx.x + neuron_execution_values_write;
+		execution_values[execution_values_i] = activation;
+	}
 }
 
 #endif

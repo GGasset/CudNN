1f21815314c4edfc0958bef2859899f55e064a0d
Germán <GassetGerman@gmail.com>
2024-04-04 16:25:33 +0200

    added tanh_activation declaration

--- neuron_operations.cuh
+++ neuron_operations.cuh
@@ -12,3 +12,9 @@ __global__ void sigmoid_activation(
 	data_t* execution_values, size_t execution_values_start, size_t execution_values_layer_start, size_t execution_values_per_neuron,
 	size_t neuron_execution_values_read, size_t neuron_execution_values_write, short write_execution_values
 );
+
+__global__ void tanh_activation(
+	data_t* activations, size_t activations_start, size_t layer_activation_start, short write_activation,
+	data_t* execution_values, size_t execution_values_start, size_t execution_values_layer_start, size_t execution_values_per_neuron,
+	size_t neuron_execution_values_read, size_t neuron_execution_values_write, short write_execution_values
+);

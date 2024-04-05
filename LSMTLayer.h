ea71db9b7f117299783f57bb805e4f8654750103
Germán <GassetGerman@gmail.com>
2024-04-05 14:47:02 +0200

    made all LSTM weights a single pointer

--- LSMTLayer.h
+++ LSMTLayer.h
@@ -1,10 +1,17 @@
 #include "ILayer.h"
 
 #pragma once
-class LSMTLayer
+class LSMTLayer : public ILayer
 {
 private:
-	parameter_t* derivatives_until_memory_deletion = 0;
+	field_t* derivatives_until_memory_deletion = 0;
 	size_t trained_steps_since_memory_deletion = 0;
+
+	field_t* neuron_weights = 0;
+
+	void execute(
+		data_t* activations, size_t activations_start,
+		data_t* execution_values, size_t execution_values_start
+	) override;
 };
 

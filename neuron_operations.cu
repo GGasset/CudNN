f137e83543806a820e2fbf0c7a5d33c048de5774
Germán <GassetGerman@gmail.com>
2024-04-05 14:47:29 +0200

    added declaration of LSTM execution

--- neuron_operations.cu
+++ neuron_operations.cu
@@ -77,4 +77,13 @@ __global__ void tanh_activation(
 	);
 }
 
+__global__ void LSTM_execution(
+	data_t* activations, size_t activations_start, size_t layer_activations_start,
+	data_t* execution_values, size_t execution_values_start, size_t execution_values_layer_start, size_t execution_values_per_neuron,
+	field_t* neuron_weights
+)
+{
+
+}
+
 #endif
--- neuron_operations.cuh
+++ neuron_operations.cuh
@@ -21,4 +21,10 @@ __global__ void tanh_activation(
 	size_t neuron_execution_values_read, size_t neuron_execution_values_write, short write_execution_values
 );
 
+__global__ void LSTM_execution(
+	data_t* activations, size_t activations_start, size_t layer_activations_start,
+	data_t* execution_values, size_t execution_values_start, size_t execution_values_layer_start, size_t execution_values_per_neuron,
+	field_t* neuron_weights
+);
+
 #endif

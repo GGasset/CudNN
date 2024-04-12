bdc20891b7127aa3abe9d39e50b50f85d5d33d30
Germán <GassetGerman@gmail.com>
2024-04-12 22:15:55 +0200

    fixed typo

--- derivatives.cu
+++ derivatives.cu
@@ -18,7 +18,7 @@ __device__ data_t device_tanh_derivative(
 	return (4 * exp_2_x) / ((exp_2_x + 1) * (exp_2_x + 1));
 }
 
-__global__ void LSMT_derivative_calculation(
+__global__ void LSTM_derivative_calculation(
 	data_t* derivatives, size_t previous_derivatives_start, size_t derivatives_start, size_t derivatives_layer_start, size_t derivatives_per_neuron,
 	data_t* execution_values, size_t execution_values_start, size_t execution_values_layer_start, size_t execution_values_per_neuron,
 	field_t* neuron_weights
--- derivatives.cuh
+++ derivatives.cuh
@@ -3,7 +3,7 @@
 
 #include "data_type.h"
 
-__global__ void LSMT_derivative_calculation(
+__global__ void LSTM_derivative_calculation(
 	data_t* derivatives, size_t previous_derivatives_start, size_t derivatives_start, size_t derivatives_layer_start, size_t derivatives_per_neuron,
 	data_t* execution_values, size_t execution_values_start, size_t execution_values_layer_start, size_t execution_values_per_neuron,
 	field_t* neuron_weights

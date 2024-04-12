d1ccecb54e6366eeeee6e4b579ad34425aed7c03
Germán <GassetGerman@gmail.com>
2024-04-12 21:34:36 +0200

    modified header files to adjust to last change

--- gradients.cuh
+++ gradients.cuh
@@ -5,7 +5,7 @@
 
 
 __global__ void LSTM_gradient_calculation(
-	data_t* derivatives, size_t derivatives_start, size_t derivatives_per_neuron,
+	data_t* derivatives, size_t derivatives_start, size_t derivatives_layer_start, size_t derivatives_per_neuron,
 	data_t* gradients, size_t gradients_start, size_t next_t_gradients_start, size_t layer_gradients_start, size_t* neuron_gradients_starts,
 	data_t* costs, size_t costs_start, size_t layer_costs_start
 );

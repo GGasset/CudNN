f304d76547cc3fa9e2e86ba73d9962101b62724c
Germán <GassetGerman@gmail.com>
2024-04-12 21:40:51 +0200

    added kernel call

--- LSMTLayer.cpp
+++ LSMTLayer.cpp
@@ -9,7 +9,7 @@ void LSMTLayer::execute(data_t* activations, size_t activations_start, data_t* e
 		execution_values_layer_start, execution_values_per_neuron,
 		weights, biases, neuron_count
 	);
-	LSTM_execution(
+	LSTM_execution kernel(1, neuron_count) (
 		activations, activations_start, layer_activations_start,
 		execution_values, execution_values_start, execution_values_layer_start, execution_values_per_neuron,
 		neuron_weights, state;

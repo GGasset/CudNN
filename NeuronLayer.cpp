6db7e347b394e2f8567e9e1ed585b185edec2187
Germán <GassetGerman@gmail.com>
2024-04-14 16:33:54 +0200

    added calculate_gradients to NeuronLayer

--- NeuronLayer.cpp
+++ NeuronLayer.cpp
@@ -32,4 +32,26 @@ void NeuronLayer::execute(
 	cudaDeviceSynchronize();
 }
 
+void NeuronLayer::calculate_gradients(
+	data_t* activations, size_t activations_start, 
+	data_t* execution_values, size_t execution_values_start,
+	data_t* derivatives, size_t previous_derivatives_start, size_t derivatives_start, short calculate_derivatives, 
+	data_t* gradients, size_t next_gradients_start, size_t gradients_start, 
+	data_t* costs, size_t costs_start)
+{
+	neuron_gradient_calculation kernel(1, neuron_count) (
+		execution_values, execution_values_start, execution_values_layer_start,
+		gradients, gradients_start, layer_gradients_start, neuron_gradients_starts,
+		costs, costs_start, layer_activations_start,
+		activation
+	);
+	cudaDeviceSynchronize();
+	connections->calculate_gradients(
+		activations, activations_start, gradients, gradients_start, layer_gradients_start, neuron_gradients_starts,
+		costs, costs_start,
+		weights
+	);
+	cudaDeviceSynchronize();
+}
+
 #endif

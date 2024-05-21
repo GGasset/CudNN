434161872ce2c91775b66db846044453bae6b325
Germán <GassetGerman@gmail.com>
2024-05-21 17:02:09 +0200

    fixed bug

--- DenseLSTMLayer.cpp
+++ DenseLSTMLayer.cpp
@@ -7,16 +7,21 @@ DenseLSTMLayer::DenseLSTMLayer(size_t layer_gradients_start, size_t neuron_count
 	this->connection_count = neuron_count * previous_layer_length;
 	execution_values_per_neuron = 10;
 
+	size_t* connection_gradient_counts = new size_t[neuron_count];
 	size_t neuron_gradient_i = layer_gradients_start;
 	size_t* neuron_gradients_starts = new size_t[neuron_count];
 	for (size_t i = 0; i < neuron_count; i++)
 	{
+		connection_gradient_counts[i] = previous_layer_length;
 		neuron_gradients_starts[i] = neuron_gradient_i;
 		neuron_gradient_i += previous_layer_length + 7;
 	}
+	cudaMalloc(&connection_associated_gradient_counts, sizeof(size_t) * neuron_count);
 	cudaMalloc(&this->neuron_gradients_starts, neuron_count * sizeof(size_t));
 	cudaDeviceSynchronize();
+	cudaMemcpy(connection_associated_gradient_counts, connection_gradient_counts, sizeof(size_t) * neuron_count, cudaMemcpyHostToDevice);
 	cudaMemcpy(this->neuron_gradients_starts, neuron_gradients_starts, neuron_count * sizeof(size_t), cudaMemcpyHostToDevice);
+	delete[] connection_gradient_counts;
 	delete[] neuron_gradients_starts;
 
 	derivatives_per_neuron = 16;

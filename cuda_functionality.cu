e54c7b4c6ca0a152c96449445ae766452129bf34
Germán <GassetGerman@gmail.com>
2024-05-24 12:30:33 +0200

    added device_min for gradient_clipping

--- cuda_functionality.cu
+++ cuda_functionality.cu
@@ -1,1 +1,6 @@
 #include "cuda_functionality.cuh"
+
+__device__ data_t device_min(data_t a, data_t b)
+{
+	return a * (a <= b) + b * (b < a);
+}
--- cuda_functionality.cuh
+++ cuda_functionality.cuh
@@ -1,5 +1,9 @@
 #pragma once
-class cuda_functionality
-{
-};
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include "data_type.h"
+
+__device__ data_t device_min(data_t a, data_t b);
 

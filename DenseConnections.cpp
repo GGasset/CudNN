504a68f9c74464c5bb7ff148411885c83c5d46d1
Germán <GassetGerman@gmail.com>
2024-05-27 22:40:25 +0200

    optimized cud_dense_linear_function for layers bigger than 1024

--- DenseConnections.cpp
+++ DenseConnections.cpp
@@ -16,7 +16,7 @@ void DenseConnections::linear_function(size_t activations_start, data_t* activat
 	data_t* execution_values, size_t execution_values_start, size_t execution_values_layer_start, size_t layer_execution_values_per_neuron,
 	field_t* weights, field_t* biases, size_t layer_length)
 {
-	cud_dense_linear_function kernel(layer_length, previous_layer_length) (
+	cud_dense_linear_function kernel(dim3(previous_layer_length / 32 + (previous_layer_length % 32 > 0), layer_length, 1), 32) (
 		previous_layer_length, weights,
 		activations_start, previous_layer_activations_start, activations,
 		execution_values_start, execution_values_layer_start, layer_execution_values_per_neuron, execution_values
--- linear_functions.cu
+++ linear_functions.cu
@@ -13,10 +13,13 @@ __global__ void cud_dense_linear_function(
 	size_t activations_start, size_t previous_layer_activations_start, data_t* activations,
 	size_t execution_values_start, size_t execution_values_layer_start, size_t layer_execution_values_per_neuron, data_t* execution_values)
 {
-	size_t connected_activation_i = activations_start + previous_layer_activations_start + threadIdx.x;
-	size_t execution_values_i = execution_values_start + execution_values_layer_start + layer_execution_values_per_neuron * blockIdx.x;
+	size_t tid = get_tid();
+	if (tid >= previous_layer_length)
+		return;
+	size_t connected_activation_i = activations_start + previous_layer_activations_start + tid;
+	size_t execution_values_i = execution_values_start + execution_values_layer_start + layer_execution_values_per_neuron * blockIdx.y;
 
-	field_t current_weight = weights[previous_layer_length * blockIdx.x + threadIdx.x];
+	field_t current_weight = weights[previous_layer_length * blockIdx.y + tid];
 	atomicAdd(execution_values + execution_values_i, current_weight * activations[connected_activation_i]);
 }
 

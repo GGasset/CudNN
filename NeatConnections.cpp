082da8b6206fc823dc90d405d44308b53e26983a
Germán <GassetGerman@gmail.com>
2024-05-28 17:04:50 +0200

    added ::calculate_gradients

--- NeatConnections.cpp
+++ NeatConnections.cpp
@@ -47,3 +47,26 @@ void NeatConnections::calculate_derivative(
 	);
 	cudaDeviceSynchronize();
 }
+
+void NeatConnections::calculate_gradients(
+	data_t* activations, size_t activations_start, 
+	data_t* gradients, size_t gradients_start, size_t layer_gradients_start, size_t* neuron_gradients_starts, 
+	data_t* costs, size_t costs_start, 
+	field_t* weights, size_t layer_length
+)
+{
+	size_t connections_start = 0;
+	for (size_t i = 0; i < layer_length; i++)
+	{
+		size_t connection_count = connection_counts[i];
+		cud_NEAT_gradient_calculation kernel(connection_count / 32 + (connection_count % 32 > 0), 32) (
+			activations, activations_start,
+			gradients, gradients_start, layer_gradients_start, neuron_gradients_starts,
+			costs, costs_start,
+			i, connection_count, weights, connection_points, connections_start
+		);
+
+		connections_start += connection_count;
+	}
+	cudaDeviceSynchronize();
+}

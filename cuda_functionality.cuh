a73e3cf7c41261e3a154cea6e6b9bc277342ee46
Germán <GassetGerman@gmail.com>
2024-06-01 13:27:30 +0200

    added get_occurrences cuda function

--- cuda_functionality.cuh
+++ cuda_functionality.cuh
@@ -13,3 +13,5 @@ __device__ data_t device_min(data_t a, data_t b);
 /// <returns></returns>
 __device__ size_t get_tid();
 
+__global__ void get_occurrences(size_t array_value_count, size_t* read_array, size_t search_value, size_t* count);
+

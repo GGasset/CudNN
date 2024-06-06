4b169ff72a2d2a80f1779e426bf3ee79b5d9eb4b
Germán <GassetGerman@gmail.com>
2024-06-06 13:33:57 +0200

    added field

--- evolution_info.h
+++ evolution_info.h
@@ -5,6 +5,7 @@ typedef struct evolution_metadata {
 	float evolution_metadata_field_max_mutation = .02;
 
 	float field_max_evolution = .2;
+	float field_mutation_chance = .1;
 	float layer_addition_probability = .05;
 	float neuron_deletion_probability = .1;
 	float neuron_addition_probability = .1;

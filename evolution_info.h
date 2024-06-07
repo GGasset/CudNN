51e13a50dfc6436bb000df10c4333e75a1de47d4
Germán <GassetGerman@gmail.com>
2024-06-07 13:22:25 +0200

    moved fields in evolution metadata

--- evolution_info.h
+++ evolution_info.h
@@ -1,13 +1,13 @@
 #pragma once
 
 typedef struct evolution_metadata {
-	float evolution_metadata_field_mutation_chance = .05;
-	float evolution_metadata_field_max_mutation = .02;
-
 	float field_max_evolution = .2;
 	float field_mutation_chance = .1;
 	float layer_addition_probability = .05;
 	float neuron_deletion_probability = .1;
 	float neuron_addition_probability = .1;
 	float layer_distance_from_added_neuron_connection_addition_modifier = .01;
-};
+
+	float evolution_metadata_field_mutation_chance = .05;
+	float evolution_metadata_field_max_mutation = .02;
+} evolution_metadata;

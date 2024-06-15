249667813a16d2abbb64796c67b80eda155dd4a4
Germán Gasset Martí <gassetgerman@gmail.com>
2024-06-15 21:59:27 +0200

    added spaces

--- LSTMLayer.cu
+++ LSTMLayer.cu
@@ -4,8 +4,12 @@ LSTMLayer::LSTMLayer(IConnections* connections, size_t neuron_count)
 {
 	this->connections = connections;
 	set_neuron_count(neuron_count);
+
 	execution_values_per_neuron = 10;
+	
 	derivatives_per_neuron = 16;
+	layer_derivative_count = derivatives_per_neuron * neuron_count;
+	
 	layer_gradient_count = 7 * neuron_count + neuron_count + connections->connection_count;
 
 	layer_specific_initialize_fields(connections->connection_count, neuron_count);

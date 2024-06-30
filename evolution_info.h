625685b1ba796ad22d9b3cbd296a89be8c9af2ef
GG <gassetgerman@gmail.com>
2024-06-30 13:33:02 +0200

    improved fields for slower evolution

--- evolution_info.h
+++ evolution_info.h
@@ -1,13 +1,13 @@
 #pragma once
 
 typedef struct evolution_metadata {
-	float field_max_evolution = .2;
-	float field_mutation_chance = .1;
-	float layer_addition_probability = .05;
-	float neuron_deletion_probability = .1;
-	float neuron_addition_probability = .1;
-	float layer_distance_from_added_neuron_connection_addition_modifier = .01;
+	float field_max_evolution = .002;
+	float field_mutation_chance = .01;
+	float layer_addition_probability = .001;
+	float neuron_deletion_probability = .01;
+	float neuron_addition_probability = .02;
+	float layer_distance_from_added_neuron_connection_addition_modifier = .001;
 
-	float evolution_metadata_field_mutation_chance = .05;
-	float evolution_metadata_field_max_mutation = .02;
+	float evolution_metadata_field_mutation_chance = .005;
+	float evolution_metadata_field_max_mutation = .001;
 } evolution_metadata;

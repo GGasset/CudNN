19c26ed1871357bbb53a7e801c176de4ad265192
GG <gassetgerman@gmail.com>
2024-07-08 16:03:03 +0200

    added max & min functions

--- functionality.h
+++ functionality.h
@@ -1,4 +1,16 @@
 #pragma once
 #include <cstdlib>
 
+template<typename t>
+t max(t a, t b)
+{
+	return a * (a >= b) + b * (a < b);
+}
+
+template<typename t>
+t min(t a, t b)
+{
+	return a * (a <= b) + b * (a > b);
+}
+
 float get_random_float();

dd99391eeb19933022f4b78189c42c36978a5f14
GGasset <gassetgerman@gmail.com>
2025-04-17 22:21:21 +0200

    modified forgotten header

--- DenseConnections.h
+++ DenseConnections.h
@@ -30,7 +30,7 @@ public:
 
 	void subtract_gradients(
 		data_t* gradients, size_t gradients_start, size_t layer_gradients_start, size_t* neuron_gradients_starts,
-		data_t learning_rate, short* dropout, data_t gradient_clip
+		data_t learning_rate, data_t gradient_clip
 	) override;
 	
 	IConnections* connections_specific_clone() override;
